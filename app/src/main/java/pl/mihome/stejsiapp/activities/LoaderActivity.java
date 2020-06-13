1282d366ee2ca8de61346fec3b7a6d27797d9b7a
milosz.kozlowski <mkozlowski@tlen.pl>
2020-06-13 20:26:53 +0200

    Main layout with recyclerview multiple models adapter added for tips, trainings date confoirmation and presence

---
+++ app/src/main/java/pl/mihome/stejsiapp/activities/LoaderActivity.java
@@ -0,0 +1,6 @@
+package pl.mihome.stejsiapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoaderActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/pl/mihome/stejsiapp/activities/MainViewAdapter.java
@@ -0,0 +1,4 @@
+package pl.mihome.stejsiapp.activities;
+
+class MainViewAdapter extends androidx.recyclerview.widget.RecyclerView.Adapter {
+}
